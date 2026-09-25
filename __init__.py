17b0af58b600766dac73d934909d9ff23a1d511f
adrien <adrien.guichard@citel.fr>
2026-09-25 09:09:05 +0200

    WiP create protobuf (it is totally broken)

--- __init__.py
+++ __init__.py
@@ -19,12 +19,12 @@ connected = None
 class TaackPlmPreferences(bpy.types.AddonPreferences):
     bl_idname = __package__ if __package__ else os.path.splitext(os.path.basename(__file__))[0]
 
-    serverUrl: bpy.props.StringProperty(
+    serverUrl: StringProperty(
         name="Server URL",
         description="The Server URL Address",
         default="http://localhost:9442/"
     )
-    username: bpy.props.StringProperty(
+    username: StringProperty(
         name="Username",
         description="Username",
         default="admin"
@@ -39,7 +39,11 @@ class TaackPlmPreferences(bpy.types.AddonPreferences):
 
 
 class TaackPlmProperties(PropertyGroup):
-    password: StringProperty(name="Password", subtype="PASSWORD", description="Password ...")
+    password: StringProperty(
+        name="Password",
+        subtype="PASSWORD",
+        description="Password ..."
+    )
 
 
 class TaackPlmConnect(Operator):
@@ -69,16 +73,124 @@ class TaackPlmConnect(Operator):
         return {"FINISHED"}
 
 
+
 class TaackPlmUpload(Operator):
     bl_label = "Upload To Server"
     bl_idname = "taack.plm_fork_upload"
     bl_description = "Upload saved model to the server"
 
+    def __init__(self):
+        self.avoidLoop = None
+        self.uuidVersion = None
+        self.docLabelsForked = None
+        self.forkMode = 'Active'
+
+
+    def create_missing_uuid(self, obj, force):
+        if force:
+            obj['taack_id'] = str(uuid.uuid4())
+        else :
+            if 'taack_id' not in obj :
+                obj['taack_id'] = str(uuid.uuid4())
+            else:
+                obj['taack_id'] = obj['taack_id'] + '/' + str(uuid.uuid4())
+        linked_objects = iter(obj.children)
+        for l in linked_objects:
+            self.create_link_protobuf(l, force)
+
+    def create_bucket_protobuf(self, obj):
+        print("createBucketProtobuf")
+        self.avoidLoop = []
+        d = bpy.data
+        c = bpy.context
+        if d.filepath != "":
+            bucket = PlmBuf.Bucket()
+            self.create_missing_uuid(c.active_object, False)
+            bpy.ops.wm.save_as_mainfile(d.filepath)
+            create_doc_protobuf(c.active_object, bucket)
+            return bucket
+        else:
+            self.report({"INFO"}, "Save the file before uploading !")
+
+        linked_objects = iter(obj.children)
+        for l in linked_objects:
+            self.create_bucket_protobuf(l)
+
+        return None
+
+    def create_doc_protobuf(self, obj, bucket):
+        print("createDocProtobuf " + obj.name)
+        try:
+            if self.avoidLoop.count(obj.name) > 0:
+                return None
+            self.avoidLoop.append(obj.name)
+            plm_file = PlmBuf.PlmFile()
+            s = os.stat(obj.filepath)
+            plm_file.cTimeNs = s.st_ctime_ns
+            plm_file.uTimeNs = s.st_mtime_ns
+            plm_file.name = obj.name
+
+            plm_file.id = obj['taack_id']
+            print("plmFile.id " + plm_file.id)
+
+            plm_file.label = obj.Label
+            plm_file.comment = obj.Comment
+            plm_file.fileName = os.path.basename(obj.filepath)
+            plm_file.createdDate = obj.CreationDate
+            plm_file.createdBy = bpy.context.preferences.filepaths.author
+            plm_file.lastModifiedDate = os.path.getctime(obj.filepath)
+            plm_file.lastModifiedBy = os.path.getctime(obj.filepath)
+            linked_objects = iter(obj.children)
+            for l in linked_objects:
+                lp = self.create_link_protobuf(l, bucket)
+                if lp is not None:
+                    plm_file.externalLink.append(lp)
+
+            plm_file.fileContent = open(obj.filepath, 'rb').read()
+            bucket.plmFiles[plm_file.name].CopyFrom(plm_file)
+        except:
+            self.report({"ERROR"}, "Error creating protobuf file !")
+
+        return obj.name
+
+    def create_link_protobuf(self, obj, bucket):
+        print("createLinkProtobuf " + obj.name)
+        try:
+            plm_link = PlmBuf.PlmLink()
+            plm_link.linkedObject = obj.name
+            plm_link.linkClaimChild = False
+            plm_link.linkCopyOnChange = PlmBuf.PlmLink.LinkCopyOnChangeEnum.Disabled
+            plm_link.linkTransform = False
+            f = create_doc_protobuf(obj.LinkedObject.Document, bucket)
+            if f is None:
+                return None
+            plm_link.plmFile = f
+            bucket.links[obj.name].CopyFrom(plm_link)
+        except:
+            raise ValueError("createLinkProtobuf Error")
+
+        return obj.name
+
     def execute(self, context):
         scene = context.scene
-        taack_props = scene.taack_props
+        if not connected:
+            self.report({"ERROR"}, "Not connected to the server")
+            return False
 
-        return {"FINISHED"}
+        b = self.create_bucket_protobuf()
+        f = open("bl_proto", 'wb')
+        f.write(b.SerializeToString())
+        f.close()
+        data = {"ajax": 'true'}
+        f2 = open("bl_proto", 'rb')
+        r = self.po.taackIntranetSession.post(url=self.po.url + 'plm/uploadProto', files={'proto.bin': f2}, data=data)
+        f2.close()
+
+        if r.json()["success"]:
+            return True
+        else:
+            self.report({"ERROR"}, "Message does not successfully sent: " + r.json()["message"])
+            return False
 
 
 class TaackPlmForkRecent(Operator):
@@ -105,8 +217,6 @@ class TAACK_PT_panel(Panel):
         scene = context.scene
         taack_props = scene.taack_props
 
-        # layout.prop(taack_props, "serverUrl")
-        # layout.prop(taack_props, "username")
         layout.prop(taack_props, "password")
         layout.separator()
         op_row_upload = layout.row()
